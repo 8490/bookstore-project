26e09f260e978adfe21d092accdd517ea30efa9b
Mert <mert.san3407@gmail.com>
2020-12-13 19:58:45 +0300

    updated server js

--- server.js
+++ server.js
@@ -12,14 +12,13 @@ app.use(express.json());
 app.use("/api", router);
 
 
-// production heroku 
-if (process.env.NODE.ENV === "production") {
+// production
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-
 
 app.listen(process.env.PORT, () => {
   console.log(`I'm listening on port ${process.env.PORT}`);
