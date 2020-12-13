79d52d410c7645a428789139619897cf61ef29e7
Mert <mert.san3407@gmail.com>
2020-12-13 19:33:04 +0300

    added build in client

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ connectDB();
 app.use(express.json());
 app.use("/api", router);
 
+
+// production heroku 
+if (process.env.NODE.ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
+
 app.listen(process.env.PORT, () => {
   console.log(`I'm listening on port ${process.env.PORT}`);
 });
